c7edb0ba06b028a4804454110a77cca802780676
minhducle291 <minhducle291@gmail.com>
2025-09-22 16:35:16 +0700

    updatee

--- handlers.py
+++ handlers.py
@@ -7,6 +7,7 @@ import os
 
 # URL public (ngrok/domain)
 PUBLIC_BASE_URL = os.getenv("PUBLIC_BASE_URL", "https://linebot-qer1.onrender.com")
+#PUBLIC_BASE_URL = os.getenv("PUBLIC_BASE_URL", "https://787e732a65b9.ngrok-free.app")
 
 def handle_user_message(user_text: str):
     """
@@ -19,7 +20,7 @@ def handle_user_message(user_text: str):
         messages.append(TextMessage(text=f"Bạn vừa nhắn: {user_text}"))
 
     # Trường hợp 2: tin nhắn chứa '!' và có số => vẽ ảnh gửi
-    elif "!" in user_text:
+    elif "/nhucau" in user_text:
         match = re.search(r"\d+", user_text)
         if match:
             store_number = int(match.group())
@@ -33,11 +34,41 @@ def handle_user_message(user_text: str):
             df_to_image(df, outfile=out_path, title=f"Thông tin chia hàng thủy sản ST: {store_number}\n(dữ liệu cập nhật ngày {ngay_cap_nhat})")
 
             img_url = urljoin(PUBLIC_BASE_URL + "/", out_path)
-            messages.append(TextMessage(text=f"Đây là bảng chia hàng thủy sản cho siêu thị {store_number}:"))
-            messages.append(ImageMessage(originalContentUrl=img_url, previewImageUrl=img_url))
+            #messages.append(TextMessage(text=f"Đây là bảng chia hàng thủy sản cho siêu thị {store_number}:"))
+            #messages.append(ImageMessage(originalContentUrl=img_url, previewImageUrl=img_url))
+            messages.append(
+                ImageMessage(
+                    original_content_url=img_url,
+                    preview_image_url=img_url
+                )
+            )
+
+    # elif "/kqkd" in user_text:
+    #     match = re.search(r"\d+", user_text)
+    #     if match:
+    #         store_number = int(match.group())
+    #         df = pd.read_parquet("data_nhapban.parquet")
+    #         tu_ngay = df['Từ ngày'].iloc[0]
+    #         den_ngay = df['Đến ngày'].iloc[0]
+    #         df = df[df["Mã siêu thị"] == store_number][["Nhóm sản phẩm","SL nhu cầu (KG)","SL lên PO (KG)","SL thực nhập (KG)","SL xuất bán (KG)","Tỉ lệ nhập/PO","Tỉ lệ bán/nhập"]]
+    #         df = df.sort_values(by=["SL thực nhập (KG)"], ascending=False)
+            
+    #         filename = f"table_kqkd_{store_number}.png"
+    #         out_path = f"static/{filename}"
+    #         df_nhapban_to_image(df, outfile=out_path, title=f"Thông tin nhập - bán hàng thủy sản ST: {store_number}\n(dữ liệu từ {tu_ngay} đến {den_ngay})")
+
+    #         img_url = urljoin(PUBLIC_BASE_URL + "/", out_path)
+    #         messages.append(TextMessage(text=f"Đây là bảng thông tin nhập - bán hàng thủy sản cho siêu thị {store_number}:"))
+    #         #messages.append(ImageMessage(originalContentUrl=img_url, previewImageUrl=img_url))
+    #         messages.append(
+    #             ImageMessage(
+    #                 original_content_url=img_url,
+    #                 preview_image_url=img_url
+    #             )
+    #         )
 
     # Trường hợp khác: trả text mặc định
     else:
-        messages.append(TextMessage(text="Hãy gửi tin nhắn có chứa '!' kèm mã siêu thị để tra cứu (Ví dụ: 7300!)"))
+        messages.append(TextMessage(text="Hãy nhập lệnh + mã siêu thị để xem báo cáo!\nVí dụ:\n/nhucau 7300\n/kqkd 7300"))
 
     return messages
--- utils.py
+++ utils.py
@@ -33,3 +33,37 @@ def df_to_image(df, outfile="static/table.png", title="Kết quả"):
     plt.savefig(outfile, dpi=200, bbox_inches="tight", pad_inches=0.2)
     plt.close()
     return outfile
+
+# def df_nhapban_to_image(df, outfile="static/table_kqkd.png", title="Kết quả"):
+#     if len(df) > 0:
+#         fig_h = 0.2 * len(df)
+#         fig, ax = plt.subplots(figsize=(14, fig_h))
+#         ax.axis("off")
+
+#         ax.text(0.5, 1.02, title, ha="center", va="bottom", fontsize=13, weight="bold", transform=ax.transAxes)
+
+#         tbl = ax.table(
+#             cellText=df.values,
+#             colLabels=df.columns,
+#             colLoc="center",
+#             cellLoc="left",
+#             colWidths=[0.2, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+#             loc="upper center"
+#         )
+#         tbl.auto_set_font_size(False)
+#         tbl.set_fontsize(10)
+#         tbl.scale(1.05, 1.15)
+
+#         for (r, c), cell in tbl.get_celld().items():
+#             if r == 0:
+#                 cell.set_facecolor("#4CAF50")
+#                 cell.set_text_props(color="white", weight="bold")
+#             elif r % 2 == 0:
+#                 cell.set_facecolor("#f7f7f7")
+
+#         os.makedirs(os.path.dirname(outfile), exist_ok=True)
+#         plt.savefig(outfile, dpi=200, bbox_inches="tight", pad_inches=0.2)
+#         plt.close()
+#         return outfile
+#     else:
+#         return print("DataFrame is empty, cannot create image.")
