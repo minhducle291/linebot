cdb864cba5c1239d83bc9a868d41d67cb6ff862b
minhducle291 <minhducle291@gmail.com>
2025-09-22 16:49:55 +0700

    update

--- handlers.py
+++ handlers.py
@@ -2,7 +2,7 @@ import re
 import pandas as pd
 from urllib.parse import urljoin
 from linebot.v3.messaging import TextMessage, ImageMessage
-from utils import df_to_image
+from utils import df_to_image, df_nhapban_to_image
 import os
 
 # URL public (ngrok/domain)
@@ -34,7 +34,7 @@ def handle_user_message(user_text: str):
             df_to_image(df, outfile=out_path, title=f"Thông tin chia hàng thủy sản ST: {store_number}\n(dữ liệu cập nhật ngày {ngay_cap_nhat})")
 
             img_url = urljoin(PUBLIC_BASE_URL + "/", out_path)
-            #messages.append(TextMessage(text=f"Đây là bảng chia hàng thủy sản cho siêu thị {store_number}:"))
+            messages.append(TextMessage(text=f"Đây là bảng chia hàng thủy sản cho siêu thị {store_number}:"))
             #messages.append(ImageMessage(originalContentUrl=img_url, previewImageUrl=img_url))
             messages.append(
                 ImageMessage(
@@ -43,29 +43,29 @@ def handle_user_message(user_text: str):
                 )
             )
 
-    # elif "/kqkd" in user_text:
-    #     match = re.search(r"\d+", user_text)
-    #     if match:
-    #         store_number = int(match.group())
-    #         df = pd.read_parquet("data_nhapban.parquet")
-    #         tu_ngay = df['Từ ngày'].iloc[0]
-    #         den_ngay = df['Đến ngày'].iloc[0]
-    #         df = df[df["Mã siêu thị"] == store_number][["Nhóm sản phẩm","SL nhu cầu (KG)","SL lên PO (KG)","SL thực nhập (KG)","SL xuất bán (KG)","Tỉ lệ nhập/PO","Tỉ lệ bán/nhập"]]
-    #         df = df.sort_values(by=["SL thực nhập (KG)"], ascending=False)
+    elif "/kqkd" in user_text:
+        match = re.search(r"\d+", user_text)
+        if match:
+            store_number = int(match.group())
+            df = pd.read_parquet("data_nhapban.parquet")
+            tu_ngay = df['Từ ngày'].iloc[0]
+            den_ngay = df['Đến ngày'].iloc[0]
+            df = df[df["Mã siêu thị"] == store_number][["Nhóm sản phẩm","SL nhu cầu (KG)","SL lên PO (KG)","SL thực nhập (KG)","SL xuất bán (KG)","Tỉ lệ nhập/PO","Tỉ lệ bán/nhập"]]
+            df = df.sort_values(by=["SL thực nhập (KG)"], ascending=False)
             
-    #         filename = f"table_kqkd_{store_number}.png"
-    #         out_path = f"static/{filename}"
-    #         df_nhapban_to_image(df, outfile=out_path, title=f"Thông tin nhập - bán hàng thủy sản ST: {store_number}\n(dữ liệu từ {tu_ngay} đến {den_ngay})")
+            filename = f"table_kqkd_{store_number}.png"
+            out_path = f"static/{filename}"
+            df_nhapban_to_image(df, outfile=out_path, title=f"Thông tin nhập - bán hàng thủy sản ST: {store_number}\n(dữ liệu từ {tu_ngay} đến {den_ngay})")
 
-    #         img_url = urljoin(PUBLIC_BASE_URL + "/", out_path)
-    #         messages.append(TextMessage(text=f"Đây là bảng thông tin nhập - bán hàng thủy sản cho siêu thị {store_number}:"))
-    #         #messages.append(ImageMessage(originalContentUrl=img_url, previewImageUrl=img_url))
-    #         messages.append(
-    #             ImageMessage(
-    #                 original_content_url=img_url,
-    #                 preview_image_url=img_url
-    #             )
-    #         )
+            img_url = urljoin(PUBLIC_BASE_URL + "/", out_path)
+            messages.append(TextMessage(text=f"Đây là bảng thông tin nhập - bán hàng thủy sản cho siêu thị {store_number}:"))
+            #messages.append(ImageMessage(originalContentUrl=img_url, previewImageUrl=img_url))
+            messages.append(
+                ImageMessage(
+                    original_content_url=img_url,
+                    preview_image_url=img_url
+                )
+            )
 
     # Trường hợp khác: trả text mặc định
     else:
